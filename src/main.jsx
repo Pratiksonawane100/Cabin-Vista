d1ee636e241a48a0c3321519714f86e5bf157110
Aniket Sonawane <aniket11sonawane@gmail.com>
2024-09-12 17:22:22 +0530

    Added all files

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // Import the Tailwind CSS file
+import App from "./App";
+import { DarkModeProvider } from "./context/DarkModeContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DarkModeProvider>
+      <App />
+    </DarkModeProvider>
+  </React.StrictMode>
+);
